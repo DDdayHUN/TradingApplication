7a0cc8263e7629c3861303381bd1920234c98dee
Mark Deubzer <mark.deubzer@gmail.com>
2026-06-25 20:34:39 +0200

    feat: add finnhub provider to Tester

--- src/main/infrastructure/finnhub/FinnhubTester.java
+++ src/main/infrastructure/finnhub/FinnhubTester.java
@@ -5,10 +5,11 @@ import java.io.IOException;
 public final class FinnhubTester {
    private final FinnhubConfig m_config;
    private final FinnhubClient m_client;
+   private final FinnhubMarketDataProvider m_provider;
 
    public void runFinnhubTester(String symbol) throws IOException, InterruptedException {
       try{
-         var quote = m_client.getQuote(symbol);
+         var quote = m_provider.getQuote(symbol);
 
          System.out.println("Symbol: " + quote.symbol());
          System.out.println("Current Price: " + quote.currentPrice());
@@ -27,6 +28,7 @@ public final class FinnhubTester {
    public FinnhubTester() {
       this.m_config = new FinnhubConfig();
       this.m_client = new FinnhubClient(m_config);
+      this.m_provider = new FinnhubMarketDataProvider(this.m_client);
    }
 
 }
