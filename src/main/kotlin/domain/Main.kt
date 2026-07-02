e6a2ab32a768284620357ecda89f9522596bb3e5
DDdayHUN <220781113+DDdayHUN@users.noreply.github.com>
2026-07-02 15:35:58 +0200

    fix: Fixed Marko Finnhub bullshit on main

--- src/main/kotlin/domain/Main.kt
+++ src/main/kotlin/domain/Main.kt
@@ -2,9 +2,11 @@ package domain
 
 import application.backtest.TradingAlgorithmBackTester
 import domain.algorithm.TradingAlgorithm
+import domain.assets.security.SecurityIdentifier
 import domain.tax.ITaxation
+import infrastructure.network.finnhub.FinnhubTester
 
-fun main() {
+suspend fun main() {
     val BT = TradingAlgorithmBackTester(
         ITaxation.HUNGARY,
         TradingAlgorithm.Type.TACPP46,
@@ -13,4 +15,15 @@ fun main() {
         20,
         24)
     BT.runBackTest()
+
+    /*
+    val FN = FinnhubTester()
+    FN.runFinnhubTester(
+        SecurityIdentifier(
+            "US0378331005",
+            "USD",
+            "Apple"
+        )
+    )
+    */
 }
