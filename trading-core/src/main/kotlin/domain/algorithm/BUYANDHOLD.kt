e60f77afc4311cb6b4c1a820d1b0cc9949c12ffb
DDdayHUN <220781113+DDdayHUN@users.noreply.github.com>
2026-08-12 19:17:25 +0200

    Feat/algorithm: Added BUYANDHOLD for baseline reasons. (#90)
    * feat/alg: Added a new algorithm for benchmarking purposes. BUYANDHOLD
    
    * feat/alg_eval: Fixed formatting issues with less than 10 years available
    
    * fix: Changed a value to false in main

---
+++ trading-core/src/main/kotlin/domain/algorithm/BUYANDHOLD.kt
@@ -0,0 +1,22 @@
+package domain.algorithm
+
+import domain.market.security.SecurityHolding
+
+//===========================================================//
+/**
+ * An implementation of [TradingAlgorithm].
+ */
+//===========================================================//
+
+internal class BUYANDHOLD: ITradingAlgorithm {
+    //===========================================================//
+    //===========================================================//
+    // Public Method(es)
+
+    override fun run(holdings: List<SecurityHolding>, allocatedCapital: Double, currentPrice: Double): TradingAlgorithm.Output {
+        val amount = (allocatedCapital / currentPrice).toInt()
+        if (amount <= 0) { return TradingAlgorithm.Output(null, null) }
+
+        return TradingAlgorithm.Output(TradingAlgorithm.Output.Buy(amount), null)
+    }
+}
