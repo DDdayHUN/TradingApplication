1478369b34116beb96971fcc3edaba1547983b17
Mark Deubzer <mark.deubzer@gmail.com>
2026-09-16 12:23:22 +0200

    feat: added more security checks for trader deletion

--- trading-core/src/main/kotlin/application/service/trader/TraderService.kt
+++ trading-core/src/main/kotlin/application/service/trader/TraderService.kt
@@ -16,6 +16,7 @@ import domain.trader.SellHolding
 import domain.trader.Trader
 import domain.trader.TradingOrder
 import exception.api.HoldingNotFoundException
+import exception.api.TraderHoldingsNotEmptyException
 import infrastructure.broker.InteractiveBrokersSession
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
@@ -191,7 +192,7 @@ class TraderService(
     override suspend fun deleteTrader(traderId: UUID) {
         val trader = traderRepository.getById(traderId).getOrThrow()
 
-        if(trader.holdings.isNotEmpty()) throw IllegalStateException("Trader with holding cannot be deleted.")
+        if(trader.holdings.isNotEmpty()) throw TraderHoldingsNotEmptyException(traderId)
 
         logger.info("Deleting trader with id: ${trader.id}")
         traderRepository.delete(traderId).getOrThrow()
